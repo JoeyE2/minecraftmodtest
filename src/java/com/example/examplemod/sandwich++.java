1374a86f51b14e5b870419a4f867e711c81c71d1
JoeyE2 <78214819+JoeyE2@users.noreply.github.com>
2021-07-09 17:34:39 -0500

    were just gonna retry

--- src/java/com/example/examplemod/sandwich++.java
+++ src/java/com/example/examplemod/sandwich++.java
@@ -1,7 +1,3 @@
 package net.joey.sandwich;
 
 import io.github.joeye2.NickNameController;
-import io.github.joeye2.CommandHandler
-
-public class NickNameController implements CommandHandler
-if ChatMessageEvent PrefixWith{'sandwich++'}
